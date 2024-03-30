99b59554dfd16e60eb72b3b3b2d05cf07dd073af
tonym <tony.marques@live.fr>
2024-03-30 12:20:33 +0100

    InvoiceDetails.tsx > Create status-bar

--- src/pages/InvoiceDetails/InvoiceDetails.tsx
+++ src/pages/InvoiceDetails/InvoiceDetails.tsx
@@ -5,13 +5,17 @@ import {
 } from "../../contexts/InvoiceContext.tsx";
 import BackButton
     from "../../components/BackButton/BackButton.tsx";
+import InvoiceItemStatus
+    from "../../InvoiceItem/components/InvoiceItemStatus/InvoiceItemStatus.tsx";
+import {InvoiceType} from "../../types/InvoiceType.ts";
+import Button from "../../components/ui/Button/Button.tsx";
 
 const InvoiceDetails = () => {
     const {id} = useParams()
     const {invoices} = useInvoiceContext()
     const navigate = useNavigate()
 
-    const invoice = invoices.find((item) => item.id === id)
+    const invoice = invoices.find((item: InvoiceType) => item.id === id)
     console.log(invoice);
 
     const handleOnClick = () => {
@@ -21,6 +25,17 @@ const InvoiceDetails = () => {
     return (
         <InvoiceDetailsStyled>
            <BackButton onClick={handleOnClick}/>
+            <div className="status-bar">
+                <div className="status-container">
+                    <span className="status">Status</span>
+                    <InvoiceItemStatus status={invoice?.status} $variant={invoice?.status}/>
+                </div>
+                <div className="button-container">
+                    <Button title="Edit" $variant="edit"/>
+                    <Button title="Delete" $variant="remove"/>
+                    <Button title="Mark as Paid" $variant="paid"/>
+                </div>
+            </div>
         </InvoiceDetailsStyled>
     );
 };
@@ -29,9 +44,39 @@ export default InvoiceDetails;
 
 const InvoiceDetailsStyled = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
     position: relative;
     min-height: 100vh;
     width: 730px;
     margin: 0 auto;
+    gap: 24px;
+    
+    .status-bar{
+        margin-top: 7px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: white;
+        padding: 24px 32px;
+        border-radius: 8px;
+        
+        .status-container{
+            display: flex;
+            gap: 20px;
+            align-items: center;
+            
+            
+            .status{
+                display: flex;
+                height: max-content;
+                color: #858BB2;
+            }
+        }
+        
+        .button-container{
+            display: flex;
+            gap: 8px;
+        }
+    }
 `;
